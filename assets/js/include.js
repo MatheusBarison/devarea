eb3f10cef17a32f72b7275a0d7a7799a8dbca4ff
MatheusBarison <matheusbarison@hotmail.com>
2025-01-30 11:07:57 -0300

    commit 30012025

--- assets/js/include.js
+++ assets/js/include.js
@@ -1,17 +1,3 @@
-
-
-//Carrega um elemento na página que invoca
-function loadHTML(elementId, filePath) {
-  fetch(filePath)
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById(elementId).innerHTML = data;
-    })
-    .catch(error => {
-      console.error("Erro ao carregar o arquivo: ", error);
-    });
-}
-
 //Ativa um link selecionado
 function setActiveLink() {
   const links = document.querySelectorAll('.nav-links li a, .sidebar ul li a');
@@ -26,20 +12,72 @@ function setActiveLink() {
   });
 }
 
+//Carrega um elemento na página que invoca
 function loadHTML(elementId, filePath) {
   fetch(filePath)
     .then(response => response.text())
     .then(data => {
       document.getElementById(elementId).innerHTML = data;
-
+      
       // Agora que o sidebar foi carregado, podemos inicializar a lógica de toggle
-      initializeSidebarToggle();
+      i//nitializeSidebarToggle();
     })
     .catch(error => {
-      console.error("Erro ao carregar o arquivo: ", error);
+      console.error("Erro ao carregar o arquivo: ", elementId);
     });
 }
 
+// Carregar os componentes
+window.onload = function() {
+  loadHTML("header", "../includes/header.html");
+  loadHTML("breadcrumbId", "../includes/breadcrumb.html");
+  loadHTML("footer", "../includes/footer.html");
+  loadHTML("sidebar", "../includes/sidebar.html");
+
+// Definir o link ativo após carregar os componentes
+ setTimeout(setActiveLink, 100); // Timeout para aguardar o carregamento
+};
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const breadcrumbContainer = document.getElementById("breadcrumb-identifier");
+  const path = window.location.pathname.split("/").filter(Boolean);
+
+  let breadcrumbHTML = `<li><a href="../home.html">Home</a></li>`;
+  let currentPath = "";
+
+  path.forEach((segment, index) => {
+    currentPath += `/${segment}`;
+    if (index === path.length - 1) {
+      breadcrumbHTML += `<li aria-current="page">${decodeURIComponent(segment)}</li>`;
+    } else {
+      breadcrumbHTML += `<li><a href="${currentPath}">${decodeURIComponent(segment)}</a></li>`;
+    }
+  });
+  console.log(breadcrumbHTML);
+  console.log(breadcrumbContainer);
+  breadcrumbContainer.innerHTML = breadcrumbHTML;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 function initializeSidebarToggle() {
   const toggleBtn = document.getElementById('toggle-btn');
   const sidebar = document.getElementById('sidebar');
@@ -57,17 +95,4 @@ function initializeSidebarToggle() {
     });
   }
 }
-
-// Carregar o cabeçalho e o rodapé
-window.onload = function() {
-  loadHTML("header", "../includes/header.html");
-  loadHTML("footer", "../includes/footer.html");
-  loadHTML("sidebar", "../includes/sidebar.html");
-
-   // Definir o link ativo após carregar os componentes
- setTimeout(setActiveLink, 100); // Timeout para aguardar o carregamento
-};
-
-console.log('here');
-
-
+*/
